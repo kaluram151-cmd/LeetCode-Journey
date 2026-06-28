0d66ca34cff52a28de7b41b2b3e1daaa3d62923e
kaluram151-cmd <b.kaluram@iitg.ac.in>
2026-06-28 21:37:29 +0530

    Time: 7 ms (25.33%), Space: 14.7 MB (31.98%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    set<vector<int>> s;
+    void helper(vector<int>& nums,int idx,int target, vector<int>& combin , vector<vector<int>>& ans)
+    {
+        if(idx==nums.size())
+        {
+            return ;
+        }
+        if(target<0) return ;
+        if(target==0)
+        {
+            if(s.find(combin)!=s.end())
+            {
+                return ;
+            }
+            else
+            {
+                ans.push_back({combin});
+                s.insert(combin);
+                return ;
+            }
+        }
+        combin.push_back(nums[idx]);
+        helper(nums,idx,target-nums[idx],combin,ans);
+        helper(nums,idx+1,target-nums[idx],combin,ans);
+        combin.pop_back();
+        helper(nums,idx+1,target,combin,ans);
+        return ;
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+       vector<int> combin;
+       vector<vector<int>> ans;
+       helper(nums, 0, target, combin, ans);
+       return ans;
+    }
+};
