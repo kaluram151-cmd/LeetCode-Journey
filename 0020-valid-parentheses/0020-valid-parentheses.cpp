4289ab2f8c144e97e4ce09ff92dbceba3497e63d
kaluram151-cmd <b.kaluram@iitg.ac.in>
2026-06-30 19:26:20 +0530

    Time: 0 ms (100%), Space: 10.2 MB (6.66%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.size()%2!=0)
+        {
+            return false;
+        }
+        stack<int> st;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+            {
+                st.push(s[i]);
+                continue;
+            }
+            if(st.size()!=0)
+            {
+                if((s[i]==')' && st.top()=='(') || (s[i]=='}' && st.top()=='{') || (s[i]==']' && st.top()=='[') )
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+                
+            }
+            else
+            {
+                return false;
+            }
+        }
+        if(st.size()!=0)
+        {
+            return false;
+        }
+        return true;
+    }
+};
