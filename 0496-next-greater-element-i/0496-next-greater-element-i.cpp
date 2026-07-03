d6f87e1b859494a430956a3a8c73fe9968e16b67
kaluram151-cmd <b.kaluram@iitg.ac.in>
2026-07-03 10:07:39 +0530

    Time: 0 ms (100%), Space: 12.8 MB (39.39%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         stack<int> st;
         int n = nums2.size();
-        vector<int> ans(n,-1);
+        unordered_map<int,int> mp;
         for(int i = nums2.size()-1;i>=0;i--)
         {
             while(!st.empty() && st.top()<=nums2[i])
@@ -12,19 +12,19 @@ public:
             }
             if(st.empty())
             {
-                ans[i] = -1;
+                mp[nums2[i]] = -1;
             }
             else
             {
-                ans[i] = st.top();
+                mp[nums2[i]] = st.top();
             }
             st.push(nums2[i]);
         }
-        unordered_map<int,int>mp;
-        for(int i = 0;i<n;i++)
-        {
-            mp[nums2[i]]=ans[i];
-        }
+        // unordered_map<int,int>mp;
+        // for(int i = 0;i<n;i++)
+        // {
+        //     mp[nums2[i]]=ans[i];
+        // }
         for(int i = 0;i<nums1.size();i++)
         {
             nums1[i] = mp[nums1[i]];
