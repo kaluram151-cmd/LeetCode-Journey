123b52d1dc0fc5ab42f7f15cfdf5009e3de28cc3
kaluram151-cmd <b.kaluram@iitg.ac.in>
2026-07-03 10:05:31 +0530

    Time: 4 ms (23.07%), Space: 12.9 MB (15.58%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -20,16 +20,14 @@ public:
             }
             st.push(nums2[i]);
         }
+        unordered_map<int,int>mp;
+        for(int i = 0;i<n;i++)
+        {
+            mp[nums2[i]]=ans[i];
+        }
         for(int i = 0;i<nums1.size();i++)
         {
-            for(int j = 0;j<nums2.size();j++)
-            {
-                if(nums1[i]==nums2[j])
-                {
-                    nums1[i]=ans[j];
-                    break;
-                }
-            }
+            nums1[i] = mp[nums1[i]];
         }
         return nums1;
     }
