ae488df9ac6b83097909c91ec990bacfacdc7eb0
kaluram151-cmd <b.kaluram@iitg.ac.in>
2026-07-13 11:05:46 +0530

    Time: 0 ms (100%), Space: 26.1 MB (57.83%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
-    vector<int> pref(vector<int>& arr)
-    {
-        int n = arr.size();
-        vector<int> prefix(n,0);
-        prefix[0] = arr[0];
-        for(int i = 1;i<n;i++)
-        {
-            prefix[i] = max(prefix[i-1],arr[i]);
-        }
-        return prefix;
-    }
-    vector<int> suff(vector<int>& arr)
-    {
-        int n = arr.size();
-        vector<int> suffix(n,0);
-        suffix[n-1] = arr[n-1];
-        for(int i = n-2;i>=0;i--)
-        {
-            suffix[i] = max(suffix[i+1],arr[i]);
-        }
-        return suffix;
-    }
     int trap(vector<int>& arr) {
-        vector<int> prefix = pref(arr);
-        vector<int> suffix = suff(arr);
-        int n = arr.size();
         int total = 0;
-        for(int i = 0;i<n;i++)
+        int lmax = 0;
+        int rmax = 0;
+        int l = 0;
+        int r = arr.size()-1;
+        while(l<r)
         {
-            if(arr[i]<suffix[i] && arr[i]<prefix[i])
+            if(arr[l]<=arr[r])
             {
-                total += (min(prefix[i],suffix[i])-arr[i]);
+                if(lmax>arr[l])
+                {
+                    total += lmax-arr[l];
+                }
+                else
+                {
+                    lmax = arr[l];
+                }
+                l++;
+            }
+            else
+            {
+                if(rmax > arr[r])
+                {
+                    total += rmax-arr[r];
+                }
+                else
+                {
+                    rmax = arr[r];
+                }
+                r--;
+                
             }
         }
         return total;
