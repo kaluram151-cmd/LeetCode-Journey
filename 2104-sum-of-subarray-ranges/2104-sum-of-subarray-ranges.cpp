6e3a2662d2550e217be9f3c324ee309622f125d1
kaluram151-cmd <b.kaluram@iitg.ac.in>
2026-07-13 22:51:28 +0530

    Time: 2 ms (78.82%), Space: 15.9 MB (43.7%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,23 +1,110 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1)
+    vector<int> nsee(vector<int>& arr)
+    {
+        int n = arr.size();
+        stack<int> st;
+        vector<int> ans(n,n);
+        for(int i = n-1;i>=0;i--)
         {
-            return 0;
-        }
-        long long sum = 0;
-        for(int i = 0; i<n;i++)
-        {
-            long long x = nums[i];
-            long long y = nums[i];
-            for(int j = i+1;j<n;j++)
+            while(!st.empty() && arr[st.top()]>=arr[i])
             {
-                x = max<long long>(nums[j], x);
-                y = min<long long>(nums[j], y);
-                sum += x-y;
+                st.pop();
             }
+            if(!st.empty())
+            {
+                ans[i] = st.top();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int> psee(vector<int>& arr)
+    {
+        int n = arr.size();
+        stack<int> st;
+        vector<int> ans(n,-1);
+        for(int i = 0;i<n;i++)
+        {
+            while(!st.empty() && arr[st.top()] > arr[i])
+            {
+                st.pop();
+            }
+            if(!st.empty())
+            {
+                ans[i] = st.top();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    long long minsum(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> pse = psee(arr);
+        vector<int> nse = nsee(arr);
+        long long sum = 0;
+        for(int i = 0;i<n;i++)
+        {
+            long long x = 1LL*(nse[i]-i);
+            long long y = 1LL*(i-pse[i]);
+            sum = (sum+((x*y*arr[i])));
         }
         return sum;
+    }
+    vector<int> nmee(vector<int>& arr)
+    {
+        int n = arr.size();
+        stack<int> st;
+        vector<int> ans(n,n);
+        for(int i = n-1;i>=0;i--)
+        {
+            while(!st.empty() && arr[st.top()]<=arr[i])
+            {
+                st.pop();
+            }
+            if(!st.empty())
+            {
+                ans[i] = st.top();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int> pmee(vector<int>& arr)
+    {
+        int n = arr.size();
+        stack<int> st;
+        vector<int> ans(n,-1);
+        for(int i = 0;i<n;i++)
+        {
+            while(!st.empty() && arr[st.top()] < arr[i])
+            {
+                st.pop();
+            }
+            if(!st.empty())
+            {
+                ans[i] = st.top();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    long long maxsum(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> pme = pmee(arr);
+        vector<int> nme = nmee(arr);
+        long long sum = 0;
+        for(int i = 0;i<n;i++)
+        {
+            long long x = 1LL*(nme[i]-i);
+            long long y = 1LL*(i-pme[i]);
+            sum = (sum+((x*y*arr[i])));
+        }
+        return sum;
+    }
+    long long subArrayRanges(vector<int>& nums) {
+        long long  maximum = maxsum(nums);
+        long long minmum = minsum(nums);
+        return maximum-minmum;
     }
 };
