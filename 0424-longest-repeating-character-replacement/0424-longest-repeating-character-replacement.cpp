99f6132351d68a4022b8f39181cac4e45d8b92e9
kaluram151-cmd <b.kaluram@iitg.ac.in>
2026-07-17 12:27:48 +0530

    Time: 4 ms (57.6%), Space: 10.8 MB (71.16%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l = 0;
+        int r = 0;
+        int maxlen = 0;
+        int n = s.length();
+        int maxfreq = 0;
+        vector<int> freq(26,0);
+        while(r<n)
+        {
+            freq[s[r]-'A']++;
+            maxfreq = max(maxfreq, freq[s[r]-'A']);
+            while((r-l+1)-maxfreq>k)
+            {
+                freq[s[l]-'A']--;
+                maxfreq = 0;
+                for(int i = 0;i<26;i++)
+                {
+                    maxfreq = max(maxfreq, freq[i]);
+                }
+                l++;
+            }
+            if((r-l+1)-maxfreq<=k)
+            {
+                maxlen = max(maxlen,r-l+1);
+            }
+            r++;
+        }
+        return maxlen;
+    }
+};
