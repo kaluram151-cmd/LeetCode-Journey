c212c3981ca687a3f2723938964b0356ce23238d
kaluram151-cmd <b.kaluram@iitg.ac.in>
2026-07-24 11:07:36 +0530

    Time: 24 ms (25.35%), Space: 20.6 MB (49.31%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0;
+        int r = 0;
+        int zump = 0;
+        while(r<n-1)
+        {
+            int far = 0;
+            for(int i = 0;i<=r;i++)
+            {
+                far = max(far,i+nums[i]);
+            }
+            l = r+1;
+            r = far;
+            zump = zump+1;
+        }
+        return zump;
+    }
+};
