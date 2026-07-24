43c5df5500fe75959213f73fcf63e9f60dbac185
kaluram151-cmd <b.kaluram@iitg.ac.in>
2026-07-24 18:23:38 +0530

    Time: 0 ms (100%), Space: 20.6 MB (49.31%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = nums.size();
         int l = 0;
         int r = 0;
         int zump = 0;
+        int n = nums.size();
+        int maxIndex = 0;
         while(r<n-1)
         {
-            int far = 0;
             for(int i = l;i<=r;i++)
             {
-                far = max(far,i+nums[i]);
+                maxIndex = max(maxIndex,i+nums[i]);
             }
             l = r+1;
-            r = far;
+            r = maxIndex;
             zump = zump+1;
         }
         return zump;
