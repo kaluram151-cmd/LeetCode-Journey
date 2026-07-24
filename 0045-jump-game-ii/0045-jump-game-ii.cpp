ee54514213c9101d0c0a92c7428958b342ad379c
kaluram151-cmd <b.kaluram@iitg.ac.in>
2026-07-24 11:09:11 +0530

    Time: 0 ms (100%), Space: 20.7 MB (29.11%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -8,7 +8,7 @@ public:
         while(r<n-1)
         {
             int far = 0;
-            for(int i = 0;i<=r;i++)
+            for(int i = l;i<=r;i++)
             {
                 far = max(far,i+nums[i]);
             }
